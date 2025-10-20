2880715f39494c05f72c7fb45bbf2c0f0699438f
liamphenson <henson.l@wustl.edu>
2025-10-19 23:53:18 -0500

    Updated linprog

--- linprog.py
+++ linprog.py
@@ -1,17 +1,20 @@
 import numpy as np
 from scipy.optimize import linprog
 
+#Supplemental code for intermediate steps for Part I (in the month of March)
+#Uses SciPy's linprog method to solve intermediate linear programming problems
+
 #Coefficients of objective function
-c = np.array([2500, 5000, 9000, 25000])
+c = np.array([2500, 5000, 10000, 25000])
 
 #Coefficient matrix for inequality constraints
-A = np.array([[-30, -80, -200, -2000], [2500, 5000, 9000, 25000]])
+A = np.array([[-30, -80, -200, -2000], [0, -1, -1, -1]])
 
 #RHS of constraints
-b = np.array([-50, 9500])
+b = np.array([-50, -1])
 
-#Bounds
-bounds = np.array([(2, None), (0, 0), (0, 0), (0, 0)])
+#Bounds (upper branch after branching at x_P with k=0: set x_P=0)
+bounds = np.array([(0, None), (0, 0), (0, None), (0, None)])
 
 #Obtaining and reporting the result
 res = linprog(c, A_ub=A, b_ub=b, bounds=bounds)
@@ -19,5 +22,5 @@ res = linprog(c, A_ub=A, b_ub=b, bounds=bounds)
 x_star = res.x
 cost = res.fun
 
-print(x_star)
-print(cost)
+print(f"Optimal solution: {x_star}")
+print(f"Minimum cost: {cost}")
